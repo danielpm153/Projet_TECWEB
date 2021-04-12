638081b0d5192e209395d65e527be9b0c1dea634
danielpm153 <daniel.bol153@gmail.com>
2021-04-12 19:55:53 +0200

    footer basico

--- views/footer.php
+++ views/footer.php
@@ -8,20 +8,36 @@
 		}
 
 		?>
+<head>
+    <link rel="stylesheet" type="text/css" href='css/footer.css'>
+</head>
+        <footer class="main-footer">
+            <div class="container_footer">
+                <div class="row_footer pt-5_footer">
+                    <div class="col-12_footer col-lg-4_footer text-center_footer text-lg-right_footer">
+                        <ul class="list-unstyled_footer">
+                            <li><a href="" class="text-muted_footer">Enseigne avec nous</a></li>
+                            <li><a href="controleur.php?action=whoweare" class="text-muted_footer">À propos de nous</a></li>
+                            <li><a href="" class="text-muted_footer">Conditions d'utilisation</a></li>
+                            <li><a href="" class="text-muted_footer">FAQ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-		<div id="pied">
-
-		<?php
-		// Si l'utilisateur est connecte, on affiche un lien de deconnexion 
-		// if (valider("connecte","SESSION"))
-		// {	
-		// 	echo "Utilisateur <b>$_SESSION[pseudo]</b> connecté <b>$_SESSION[isAdmin]</b> depuis <b>$_SESSION[heureConnexion]</b> &nbsp; "; 
-		// 	echo "<a href=\"controleur.php?action=Logout\">Se Déconnecter</a>";
-		// }
-		//
-        echo "<a href=\"controleur.php?action=whoweare\">à propos de nous</a>"
-        ?>
-		</div>
+            <div class="container_footer">
+                <div class="row_footer py-3_footer">
+                    <div class="col-12_footer text-center_footer col-md-6_footer text-lg-left_footer">
+                        <small class="text-muted_footer">&copy; 2021 Tous les droits sont réservés</small>
+                    </div>
+                    <div class="col-12_footer text-center_footer col-md-6_footer text-lg-right_footer">
+                        <a href="">
+                            <small class="text-muted_footer">Politique de confidentialité</small>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
 	</body>
 </html>
--- views/whoweare.php
+++ views/whoweare.php
@@ -3,9 +3,9 @@
 
 ?>
 <head>
-<!-- BOOTSTRAP -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-      integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href='css/whoweare.css'>
 </head>
 
